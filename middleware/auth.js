e3aecc22618dcc9ef3b7bc89e227eca6562b79fc
Pawan Jatav <pawanjatav6267@gmail.com>
2026-05-10 18:27:05 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,10 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = async (req, res, next) => {
+  // const authHeader = req.headers.authorization;
+
+  console.log(req.headers);
+
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
